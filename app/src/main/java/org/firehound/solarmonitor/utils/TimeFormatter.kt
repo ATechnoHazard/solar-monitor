74655f9ac83837a9454b3cfcd8e0aab29ea0e569
ATechnoHazard <amolele@gmail.com>
2019-07-04 05:13:37 -0700

    Fix timezone in timeformatter
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/org/firehound/solarmonitor/utils/TimeFormatter.kt
+++ app/src/main/java/org/firehound/solarmonitor/utils/TimeFormatter.kt
@@ -8,8 +8,8 @@ import java.util.*
 class TimeFormatter : IAxisValueFormatter {
     override fun getFormattedValue(value: Float, axis: AxisBase?): String {
         val date = Date((value*10000*1000).toLong())
-        val jsf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
-        jsf.timeZone = TimeZone.getTimeZone("Asia/Kolkata")
+        val jsf = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+        jsf.timeZone = TimeZone.getTimeZone("GMT")
         return jsf.format(date)
     }
 }
