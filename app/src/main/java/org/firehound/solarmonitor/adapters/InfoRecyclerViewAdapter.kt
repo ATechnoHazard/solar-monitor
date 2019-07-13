b44a91f24cb5e34d0120a8566564917770ee4181
ATechnoHazard <amolele@gmail.com>
2019-07-13 19:54:28 +0530

    InfoAdapter: Refactor to be more idiomatic
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/org/firehound/solarmonitor/adapters/InfoRecyclerViewAdapter.kt
+++ app/src/main/java/org/firehound/solarmonitor/adapters/InfoRecyclerViewAdapter.kt
@@ -9,13 +9,9 @@ import org.firehound.solarmonitor.R
 
 class InfoRecyclerViewAdapter (private val title : ArrayList<String>, private val subtitle : ArrayList<String>) :
     RecyclerView.Adapter<InfoRecyclerViewAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.main_recyclerview_item, parent, false))
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.main_recyclerview_item, parent, false))
 
-    override fun getItemCount(): Int {
-        return title.size
-    }
+    override fun getItemCount() = title.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.titleText.text = title[position]
